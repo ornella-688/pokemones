070b0d59ae4e8206df1d55d6fa5b312a47489c4f
ornella-688 <ornella.cicalello@gmail.com>
2022-01-06 17:51:06 -0300

    page card creation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import React from "react";
 import Home from './components/home/Home';
+import PageCard from './components/pageCard/PageCard';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-     <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/details/:id" element={<PageCard/>} />
+        </Routes>
+    </BrowserRouter>
+     
     </div>
   );
 }
